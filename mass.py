93fe54466994ddde42ef372125424dcc2264fca6
bintangAlif5 <49284528+bintangAlif5@users.noreply.github.com>
2019-08-16 04:02:21 +0700

    Update mass.py

--- mass.py
+++ mass.py
@@ -1,29 +1,25 @@
- import re,sys,random,time,os
+import re,sys,time,os,random
 
-def importt():
- try:
+try:
    import requests
- except ImportError:
+except ImportError:
     print '---------------------------------------------------'
     print '  \033[00m[\033[91m*\033[00m] \033[92m pip install requests'
     print '   \033[00m[\033[91m-\033[00m]\033[92m  you need to install requests Module\033[00m'
     sys.exit()
- else:
-    mulai()
-def mulai():
- try:
-    os.system("clear")
-    time.sleep(0.9)
-    print "=====================================================\n"
-    os.system("figlet Mass | lolcat")
-    print
-    print "\033[96mAuthor\033[00m:\033[95m r00t@star\033[00m"
-    print "\033[96mTeam\033[00m:\033[95m Sunda Cyber Army"
-    print "\033[96mInfo\033[00m:\033[95m Mass Mirror Hackersid"
-    print "\033[96mNote\033[00m:\033[95m If Error Please Contact Me\033[93m(in Readme.md)\033[00m"
-    print "=====================================================\n"
-    nick = raw_input("\033[92mNick anda:\033[00m ")
-    team = raw_input("\033[92mTeam anda:\033[00m ")
+
+os.system("clear")
+time.sleep(0.9)
+print "=====================================================\n"
+os.system("figlet Mass | lolcat")
+print
+print "\033[96mAuthor\033[00m:\033[95m r00t@star\033[00m"
+print "\033[96mTeam\033[00m:\033[95m Sunda Cyber Army"
+print "\033[96mInfo\033[00m:\033[95m Mass Mirror Hackersid"
+print "\033[96mNote\033[00m:\033[95m If Error Please Contact Me\033[93m(in Readme.md)\033[00m"
+print "=====================================================\n"
+nick = raw_input("\033[92mNick anda:\033[00m ")
+team = raw_input("\033[92mTeam anda:\033[00m ")
 team1 = team.replace(" ","+")
 hasil = raw_input("\033[92mUrl to list (ext : list.txt) :\033[00m ")
 fil = open(hasil,"r").readlines()
@@ -46,4 +42,3 @@ for i in range(jml):
 	buka(url)
 	time.sleep(2)
 
-importt()
