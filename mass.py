8cbe2285c8922ef9f07b06ffe2a23cf55e354616
bintangAlif5 <49284528+bintangAlif5@users.noreply.github.com>
2019-08-19 20:26:53 +0700

    Update mass.py

--- mass.py
+++ mass.py
@@ -12,7 +12,6 @@ os.system("clear")
 time.sleep(0.9)
 print "=====================================================\n"
 os.system("figlet Mass | lolcat")
-print
 print "\033[96mAuthor\033[00m:\033[95m r00t@star\033[00m"
 print "\033[96mTeam\033[00m:\033[95m Sunda Cyber Army"
 print "\033[96mInfo\033[00m:\033[95m Mass Mirror Hackersid"
@@ -22,8 +21,12 @@ nick = raw_input("\033[92mNick anda:\033[00m ")
 team = raw_input("\033[92mTeam anda:\033[00m ")
 team1 = team.replace(" ","+")
 hasil = raw_input("\033[92mUrl to list (ext : list.txt) :\033[00m ")
-fil = open(hasil,"r").readlines()
-jml = len(fil)
+try:
+    fil = open(hasil,"r").readlines()
+    jml = len(fil)
+except IOError:
+         print "\033[00m[\033[91m!\033[00m]\033[91mYou file not haven\033[00m"
+         sys.exit()
 for i in range(jml):
 	url = "https://hackersid.com/api/mirrorNew?hacker=%s&team=%s&url=%s&poc=Not+available&reason=Not+available" %(nick,team1,fil[i])
 	def buka(url):
